f881d7e684857f26860889d9606d8d163ef98ddb
Keval <ganganikeval@gmail.com>
2021-10-15 23:14:51 +0530

    Bull Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,25 +3,31 @@ import { Module } from '@nestjs/common';
 import { RouterModule } from '@nestjs/core';
 import { AudioModule } from './modules/audio/audio.module';
 import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 
 const { NODE_ENV } = process.env;
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      redis: {
-        host: 'localhost',
-        port: 6379,
-      },
-    }),
     ConfigModule.forRoot({
       envFilePath: [
         join(process.cwd(), 'env', `.${NODE_ENV || 'dev'}.env`),
         join(process.cwd(), 'env', '.env'),
       ],
       isGlobal: true,
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          redis: {
+            host: configService.get<string>('REDIS_HOST'),
+            port: +configService.get<number>('REDIS_PORT'),
+          },
+          inject: [ConfigService],
+        }
+      }
     }),
     AudioModule, AuthModule,
     RouterModule.register([
