5ee1a2e6b6e01b9660256a1adcdb26165875e993
Keval Gangani <ganganikeval@gmail.com>
2021-10-15 16:17:12 +0530

    Api Document Swagger

--- src/modules/audio/audio.controller.ts
+++ src/modules/audio/audio.controller.ts
@@ -1,19 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller({
-  version: '1',
-})
+@Controller({ version: '1' })
+@ApiTags('Audio')
 export class AudioController {
   constructor(@InjectQueue('audio') private readonly audioQueue: Queue) {}
 
   @Post()
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({ description: 'Job has been successfully created.'})
   async audio() {
-    return [
-      await this.audioQueue.add('audio', {
-        file: 'audio.mp3',
-      }),
-    ];
+    return await this.audioQueue.add('audio', {
+      file: 'audio.mp3',
+    });
   }
 }
